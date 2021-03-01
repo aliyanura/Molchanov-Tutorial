f5762f8226a455642f32246130cc1608dfdbe0f9
lolitagupta <aliyanura9@gmail.com>
2021-03-01 13:12:14 +0600

    sixth commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list'),
-    path('post/<str:slug>/', post_detail, name='post_detail'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail'),
     path('tags/', tags_list, name='tags_list'),
-    path('tag/<str:slug>', tag_detail, name='tag_detail')
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 from .models import Post, Tag
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', locals())
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', locals())
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', locals())
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', locals())
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
