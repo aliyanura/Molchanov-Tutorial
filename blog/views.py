4a0a0c89e853ee6d6bbd6c2225fc9df46a92aca6
lolitagupta <aliyanura9@gmail.com>
2021-03-01 10:51:49 +0600

    fourht commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def posts_list(request):
-    n='oleg'
-    return render(request, 'blog/index.html', context={"name":n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={"posts":posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', locals())
+    
