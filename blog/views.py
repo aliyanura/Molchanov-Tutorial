2a7a50f1a91192ba7b989d619d5ca412dfc02527
lolitagupta <aliyanura9@gmail.com>
2021-03-01 10:09:34 +0600

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def posts_list(request):
-    return HttpResponse('<h1>hello world</h1>')
+    n='oleg'
+    return render(request, 'blog/index.html', context={"name":n})
