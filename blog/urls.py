7f059e54869acaaefa09b180817d87231bfccb47
lolitagupta <aliyanura9@gmail.com>
2021-03-01 14:03:20 +0600

    sventh commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', posts_list, name='posts_list'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail'),
     path('tags/', tags_list, name='tags_list'),
-    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail')
+    path('tag/create/', TagCreate.as_view(), name='tag_create'),
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
+from .forms import *
 
 
 def posts_list(request):
@@ -22,3 +23,16 @@ def tags_list(request):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', locals())
+    
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', {'form': bound_form})
