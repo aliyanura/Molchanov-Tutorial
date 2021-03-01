42960ea92a8b12ab1152ac51e540f43dd7425c89
lolitagupta <aliyanura9@gmail.com>
2021-03-01 15:52:45 +0600

    eighth commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list'),
+    path('post/create/', PostCreate.as_view(), name='post_create'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail'),
     path('tags/', tags_list, name='tags_list'),
     path('tag/create/', TagCreate.as_view(), name='tag_create'),
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
+from .utils import *
 from .forms import *
 
 
@@ -25,14 +25,11 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', locals())
-    
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', {'form': bound_form})
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+
+
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template =  'blog/post_create.html'
